3206f9a3192ebed3fa87ff9f205339712c0408a1
root <root@DESKTOP-IILKFNI.localdomain>
2020-12-14 14:52:05 +0100

    exo

---
+++ doctorStrange/src/main/java/com/wcs/doctorStrange/DoctorStrangeApplication.java
@@ -0,0 +1,13 @@
+package com.wcs.doctorStrange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorStrangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorStrangeApplication.class, args);
+	}
+
+}
---
+++ doctorStrange/src/test/java/com/wcs/doctorStrange/DoctorStrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcs.doctorStrange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorStrangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
